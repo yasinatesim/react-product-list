f56c15c8cdf1ed7557c2834d58cbcbd81f019418
Yasin ATEŞ <yasinatesim@gmail.com>
2020-08-31 09:36:01 +0300

    chore: update header grid

--- client/src/containers/product-list/index.js
+++ client/src/containers/product-list/index.js
@@ -33,20 +33,22 @@ function ProductList() {
   };
 
   const renderLikedProducts = () => {
-    return likedProducts.length > 0 ? likedProducts.map(({ id, name, image, price, url, cargo, liked }) => (
-      <Column key={id}>
-        <ProductItem
-          name={name}
-          image={image}
-          price={price}
-          url={url}
-          cargoType={cargo.type}
-          cargoName={cargo.name}
-          liked={liked}
-          id={id}
-        />
-      </Column>
-    )) : 'Ürün beğenmediniz.';
+    return likedProducts.length > 0
+      ? likedProducts.map(({ id, name, image, price, url, cargo, liked }) => (
+          <Column key={id}>
+            <ProductItem
+              name={name}
+              image={image}
+              price={price}
+              url={url}
+              cargoType={cargo.type}
+              cargoName={cargo.name}
+              liked={liked}
+              id={id}
+            />
+          </Column>
+        ))
+      : 'Ürün beğenmediniz.';
   };
 
   const renderProducts = () => {
@@ -70,23 +72,22 @@ function ProductList() {
 
   return (
     <Container>
-      <br/>
-      <Row>
-        <Column>
-          <Counter>
-            <HeartIcon fill="#3498db" />
-            <span>
-              <strong>{likedProducts.length}</strong> ürün
-            </span>
-          </Counter>
-        </Column>
-        <Column>
-          <Button showLikedProducts={showLikedProducts} type="button" onClick={handleToggleLikedProducts}>
+      <Header>
+        <Counter>
+          <HeartIcon fill="#3498db" />
+          <span>
+            <strong>{likedProducts.length}</strong> ürün
+          </span>
+        </Counter>
+        <Title>
+          <h3>İlginizi Çekebilecek Ürünler</h3>
+        </Title>
+        <Filter>
+          <button showLikedProducts={showLikedProducts} type="button" onClick={handleToggleLikedProducts}>
             Beğendiklerim
-          </Button>
-        </Column>
-      </Row>
-      <br/>
+          </button>
+        </Filter>
+      </Header>
 
       <Row>{showLikedProducts ? renderLikedProducts() : renderProducts()}</Row>
     </Container>
@@ -97,22 +98,76 @@ function ProductList() {
  * Styles
  * ------------
  */
-const Button = styled.button`
-  border-radius: 4px;
+const Header = styled.div`
+  margin-top: 16px;
+  margin-bottom: 16px;
+  align-items: center;
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-areas:  'title title title'
+                        'counter counter filter';
 
-  ${({ showLikedProducts }) =>
-    showLikedProducts &&
-    css`
-      border: 2px solid #3498db;
-      color: #3498db;
-    `}
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: none;
+    grid-template-areas: 'counter title filter';
+  }
+`;
+
+const Title = styled.div`
+grid-area: title;
+margin-bottom: 24px;
+  h3 {
+    position: relative;
+    text-align: center;
+
+    &::after {
+      content: '';
+      width: 60px;
+      height: 2px;
+      position: absolute;
+      bottom: -16px;
+      left: 50%;
+      transform: translateX(-50%);
+      margin-bottom: 8px;
+      background-color: #000;
+    }
+  }
+
+  @media (min-width: 576px) {
+    margin-bottom: 0;
+  }
+`;
+
+const Filter = styled.div`
+  margin-left: auto;
+  grid-area: filter;
+
+  button {
+    border-radius: 4px;
+    font-weight: 700;
+
+    ${({ showLikedProducts }) =>
+      showLikedProducts &&
+      css`
+        border: 2px solid #3498db;
+        color: #3498db;
+      `}
+  }
+
+  @media (min-width: 576px) {
+  }
 `;
 
 const Counter = styled.div`
   display: flex;
   align-items: center;
+  grid-area: counter;
   span {
-    margin-left: 4px;
+    margin-left: 8px;
+  }
+
+  @media (min-width: 576px) {
   }
 `;
 
