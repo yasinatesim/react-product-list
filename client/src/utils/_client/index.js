3a8c2c3c0dde262da2f449adb83d4ebafeed04b6
Yasin ATEŞ <yasinatesim@gmail.com>
2020-09-01 20:27:11 +0300

    fix: deployment uri

--- client/src/utils/_client/index.js
+++ client/src/utils/_client/index.js
@@ -1,11 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import dotenv from 'dotenv';
-
-// Config
-dotenv.config();
 
 const client = new ApolloClient({
-  uri: process.env.SERVER_URI,
+  uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
 });
 
