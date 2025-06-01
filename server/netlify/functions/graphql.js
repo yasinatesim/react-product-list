26bdae6785827f2da5c53f4c08c7d304ecc62f51
Yasin ATEŞ <yasinatesim@gmail.com>
2025-06-01 11:38:49 +0300

    feat: update for netlify migration server

--- server/netlify/functions/graphql.js
+++ server/netlify/functions/graphql.js
@@ -2,4 +2,11 @@ const { createApolloServer } = require('../../src/graphqlServer');
 
 const server = createApolloServer();
 
-exports.handler = server.createHandler();
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+    methods: 'GET,POST,OPTIONS',
+  },
+});
